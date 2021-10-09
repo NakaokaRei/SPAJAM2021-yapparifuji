08c4c3c1a3bf86bdcf9711c1f5c13e2873a466d7
losiz17 <ripicea@gmail.com>
2021-10-10 06:46:32 +0900

    [Add] あとは数字を調整する

--- SPAJAM2021-yapparifuji/ViewModel/ChuckViewModel.swift
+++ SPAJAM2021-yapparifuji/ViewModel/ChuckViewModel.swift
@@ -12,13 +12,13 @@ import SoundAnalysis
 
 class ChuckViewModel: ObservableObject {
     @Published var className: String = "noise"
-    @Published var confidence = 0.0
     @Published var chuckConf = 0.0
     @Published var waterConf = 0.0
     @Published var noiseConf = 0.0
     private let notificationModel = NotificationModel()
     var classList: [String] = []
     let mlSoundManager = MLSoundManager()
+    var classificationList: [String] = [] //こんなところに
 
     init() {
         mlSoundManager.resultsObserver.delegate = self
@@ -39,31 +39,55 @@ extension ChuckViewModel: ClassifierDelegate {
     func displayPredictionResult(identifier: String, confidence: Double, classifications: [SNClassification]) {
         DispatchQueue.main.async { [self] in
             self.className = identifier
-            self.confidence = confidence
-            if classifications[0].identifier == "chack" {
-                self.chuckConf = classifications[0].confidence*100
-            } else if classifications[0].identifier == "water" {
-                self.waterConf = classifications[0].confidence*100
-            } else if classifications[0].identifier == "noise" {
-                self.noiseConf = classifications[0].confidence*100
-            }
-
-            if classifications[1].identifier == "chack" {
-                self.chuckConf = classifications[1].confidence*100
-            } else if classifications[1].identifier == "water" {
-                self.waterConf = classifications[1].confidence*100
-            } else if classifications[1].identifier == "noise" {
-                self.noiseConf = classifications[1].confidence*100
-            }
-
-            if classifications[2].identifier == "chack" {
-                self.chuckConf = classifications[2].confidence*100
-            } else if classifications[2].identifier == "water" {
-                self.waterConf = classifications[2].confidence*100
-            } else if classifications[2].identifier == "noise" {
-                self.noiseConf = classifications[2].confidence*100
-            }
-            print(self.className)
+            
+            checkClassName(listNum: 0, classifications: classifications)
+            checkClassName(listNum: 1, classifications: classifications)
+            checkClassName(listNum: 2, classifications: classifications)
+            
+           
         }
+    }
+    
+    func checkClassName(listNum: Int,classifications:[SNClassification] ) {
+        if classifications[listNum].identifier == "chack" {
+            self.chuckConf = classifications[listNum].confidence*100
+            operateList(confidence: classifications[listNum].confidence, name: classifications[listNum].identifier)
+        } else if classifications[listNum].identifier == "water" {
+            self.waterConf = classifications[listNum].confidence*100
+            operateList(confidence: classifications[listNum].confidence, name: classifications[listNum].identifier)
+        } else if classifications[listNum].identifier == "noise" {
+            self.noiseConf = classifications[listNum].confidence*100
+            operateList(confidence: classifications[listNum].confidence, name: classifications[listNum].identifier)
+        }
+    }
+    
+    func operateList(confidence: Double, name: String){
+        var validNoise: Int = 0
+        var validWater: Int = 0
+        var validChuck: Int = 0
+        //100%の時だけバッファリング，リストの長さが20超えたら最初消してバッファリング
+        if confidence*100 > 90 && self.classificationList.count <= 20{
+            self.classificationList.append(name)
+        }else if confidence*100 > 90{
+            self.classificationList.removeFirst()
+            self.classificationList.append(name)
+        }
+        print(confidence*100, self.classificationList)
+        
+        // noise -> water -> chuckの順になった時に通知を発行する (listの数が20ないと判定できない)
+        if (classificationList.count >= 20){
+            
+            validNoise = classificationList[1...10].filter({$0 == "noise"}).count
+            validWater = classificationList[6...15].filter({$0 == "water"}).count
+            validChuck = classificationList[13...19].filter({$0 == "chack"}).count
+            
+            print(validNoise, validWater, validChuck)
+        }
+        
+        //ここでめちゃくちゃ良しなに判定してほしい
+        if (validNoise > 3 && validWater > 4 && validChuck > 4){
+            self.notification()
+        }
+        
     }
 }
